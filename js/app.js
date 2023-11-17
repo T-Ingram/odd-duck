f8baef6f761bbebfb0ff818a0245effd053e5afc
Trevor Ingram <ingramtrevor10@gmail.com>
2023-11-17 10:05:13 -0800

    Created event listener for 25 rounds.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+//Global Variables
+let selectedImages = [];
+const imagesToDisplay = 3;
+const roundsDisplayed = 25;
+
 class Image {
   static generatedImages = [];
 
@@ -15,30 +20,29 @@ class Image {
   }
 }
 
-const bag = new Image('bag', 'img/bag.jpg');
-const banana = new Image('banana', 'img/banana.jpg');
-const bathroom = new Image('bathroom', 'img/bathroom.jpg');
-const boots = new Image('boots', 'img/boots.jpg');
-const breakfast = new Image('breakfast', 'img/breakfast.jpg');
-const bubblegum = new Image('bubblegum', 'img/bubblegum.jpg');
-const chair = new Image('chair', 'img/chair.jpg');
-const cthulhu = new Image('cthulhu', 'img/cthulhu.jpg');
-const dogDuck = new Image('dog-duck', 'img/dog-duck.jpg');
-const dragon = new Image('dragon', 'img/dragon.jpg');
-const pen = new Image('pen', 'img/pen.jpg');
-const petSweep = new Image('pet-sweep', 'img/pet-sweep.jpg');
-const scissors = new Image('scissors', 'img/scissors.jpg');
-const shark = new Image('shark', 'img/shark.jpg');
-const sweep = new Image('sweep', 'img/sweep.png');
-const tauntaun = new Image('tauntaun', 'img/tauntaun.jpg');
-const unicorn = new Image('unicorn', 'img/unicorn.jpg');
-const waterCan = new Image('water-can', 'img/water-can.jpg');
-const wineGlass = new Image('wine-glass', 'img/wine-glass.jpg');
+const bag = new Image('bag', '/img/bag.jpg');
+const banana = new Image('banana', '/img/banana.jpg');
+const bathroom = new Image('bathroom', '/img/bathroom.jpg');
+const boots = new Image('boots', '/img/boots.jpg');
+const breakfast = new Image('breakfast', '/img/breakfast.jpg');
+const bubblegum = new Image('bubblegum', '/img/bubblegum.jpg');
+const chair = new Image('chair', '/img/chair.jpg');
+const cthulhu = new Image('cthulhu', '/img/cthulhu.jpg');
+const dogDuck = new Image('dog-duck', '/img/dog-duck.jpg');
+const dragon = new Image('dragon', '/img/dragon.jpg');
+const pen = new Image('pen', '/img/pen.jpg');
+const petSweep = new Image('pet-sweep', '/img/pet-sweep.jpg');
+const scissors = new Image('scissors', '/img/scissors.jpg');
+const shark = new Image('shark', '/img/shark.jpg');
+const sweep = new Image('sweep', '/img/sweep.png');
+const tauntaun = new Image('tauntaun', '/img/tauntaun.jpg');
+const unicorn = new Image('unicorn', '/img/unicorn.jpg');
+const waterCan = new Image('water-can', '/img/water-can.jpg');
+const wineGlass = new Image('wine-glass', '/img/wine-glass.jpg');
 
 function getRandomImages() {
-  const selectedImages = [];
 
-  while (selectedImages.length < 3) {
+  while (selectedImages.length < imagesToDisplay) {
     const randomIndex = Math.floor(Math.random() * Image.generatedImages.length);
     const randomImage = Image.generatedImages[randomIndex];
 
@@ -47,66 +51,53 @@ function getRandomImages() {
       randomImage.timesShown++; // Increment timesShown when selected for display
     }
   }
-
-  return selectedImages.map(image => image.path);
 }
 
-function displayImages(imagePaths) {
+function displayImages() {
+  getRandomImages();
   const imageSection = document.getElementById('images');
   imageSection.innerHTML = '';
 
-  imagePaths.forEach(path => {
+  selectedImages.forEach(product => {
     const imgElement = document.createElement('img');
+    let path = product.path;
     imgElement.src = path;
-    imgElement.alt = 'Product Image';
+    imgElement.alt = product.name;
     imgElement.classList.add('product-image');
     imgElement.addEventListener('click', handleImageClick);
     imageSection.appendChild(imgElement);
-
-    const displayedImage = Image.generatedImages.find(image => image.path === path);
-    if (displayedImage) {
-      displayedImage.timesShown++;
-      // Update timesClicked for newly displayed images based on previous clicks
-      imgElement.setAttribute('data-clicked', displayedImage.timesClicked);
-      console.log(`${displayedImage.name} - Times Clicked: ${displayedImage.timesClicked}`);
-    }
   });
 }
 
 function handleImageClick(event) {
   event.preventDefault();
-
-  const clickedImagePath = event.target.src;
-  console.log('Clicked Image Path:', clickedImagePath); // Add this line for debugging
-
-  // Check if the path exists in generatedImages
-  console.log('Generated Images:', Image.generatedImages.map(image => image.path)); // Add this line for debugging
-  Image.generatedImages.forEach(image => {
-    console.log(`Clicked Path: ${clickedImagePath} | Image Path: ${image.path}`);
-  });
-
-  const clickedImage = Image.generatedImages.find(image => image.path === clickedImagePath);
-  console.log('Clicked Image:', clickedImage); // Add this line for debugging
-
-  if (clickedImage) {
-    clickedImage.timesClicked++;
-    // Log the timesClicked property of the clicked image
-    console.log(`${clickedImage.name} - Times Clicked: ${clickedImage.timesClicked}`);
+  const targetName = event.target.alt;
+  const foundObject = selectedImages.find(obj => obj.name === targetName);
+  
+  if (roundsDisplayed > 0) {
+      foundObject.timesClicked++;
+      roundsDisplayed--; // Decreasing the click limit by 1
+      console.log(`Remaining clicks: ${roundsDisplayed}`);
+      
+      if (roundsDisplayed === 0) {
+          // Disable further clicks
+          console.log('Click limit reached. Further clicks disabled.');
+          disableClicks();
+      }
+  } else {
+      console.log('Click limit reached. Further clicks disabled.');
+      disableClicks();
   }
-
-  // For now, just log the clicked image object itself to verify it's being found correctly
-  console.log('Clicked Image:', clickedImage);
-
-  const randomImagePaths = getRandomImages();
-  displayImages(randomImagePaths);
+  
+  selectedImages = [];
+  displayImages();
+  console.log(foundObject.timesClicked);
+}
+function disableClicks() {
+  const images = document.querySelectorAll('.product-image'); //The problem lies here. Replace with image class name.
+  images.forEach(image => {
+      image.removeEventListener('click', handleImageClick);
+  });
 }
 
-try {
-  const imageSection = document.getElementById('images');
-  imageSection.addEventListener('click', handleImageClick);
-} catch (error) {
-  console.error("Error adding event listener:", error);
-}
-
-const randomImagePaths = getRandomImages();
-displayImages(randomImagePaths);
+displayImages();

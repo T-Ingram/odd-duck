73d80ced5b45d5fd0eafe2e8e3f2899dca7d232c
Trevor Ingram <ingramtrevor10@gmail.com>
2023-11-20 15:44:20 -0800

    Incremented clicks...just too fast.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 //Global Variables
 let selectedImages = [];
 const imagesToDisplay = 3;
-const roundsDisplayed = 25;
+let roundsDisplayed = 25;
+generatedImages = [];
 
 class Image {
-  static generatedImages = [];
+
 
   constructor(name, path) {
     // Arguments
@@ -12,39 +13,38 @@ class Image {
     this.path = path;
     this.timesViewed = 0;
     this.timesClicked = 0;
-    Image.generatedImages.push(this);
-    
-
-    // Default variable tied to instances
     this.timesShown = 0;
+    generatedImages.push(this);
+
   }
 }
 
-const bag = new Image('bag', '/img/bag.jpg');
-const banana = new Image('banana', '/img/banana.jpg');
-const bathroom = new Image('bathroom', '/img/bathroom.jpg');
-const boots = new Image('boots', '/img/boots.jpg');
-const breakfast = new Image('breakfast', '/img/breakfast.jpg');
-const bubblegum = new Image('bubblegum', '/img/bubblegum.jpg');
-const chair = new Image('chair', '/img/chair.jpg');
-const cthulhu = new Image('cthulhu', '/img/cthulhu.jpg');
-const dogDuck = new Image('dog-duck', '/img/dog-duck.jpg');
-const dragon = new Image('dragon', '/img/dragon.jpg');
-const pen = new Image('pen', '/img/pen.jpg');
-const petSweep = new Image('pet-sweep', '/img/pet-sweep.jpg');
-const scissors = new Image('scissors', '/img/scissors.jpg');
-const shark = new Image('shark', '/img/shark.jpg');
-const sweep = new Image('sweep', '/img/sweep.png');
-const tauntaun = new Image('tauntaun', '/img/tauntaun.jpg');
-const unicorn = new Image('unicorn', '/img/unicorn.jpg');
-const waterCan = new Image('water-can', '/img/water-can.jpg');
-const wineGlass = new Image('wine-glass', '/img/wine-glass.jpg');
-
+const bag = new Image('bag', './img/bag.jpg');
+const banana = new Image('banana', './img/banana.jpg');
+const bathroom = new Image('bathroom', './img/bathroom.jpg');
+const boots = new Image('boots', './img/boots.jpg');
+const breakfast = new Image('breakfast', './img/breakfast.jpg');
+const bubblegum = new Image('bubblegum', './img/bubblegum.jpg');
+const chair = new Image('chair', './img/chair.jpg');
+const cthulhu = new Image('cthulhu', './img/cthulhu.jpg');
+const dogDuck = new Image('dog-duck', './img/dog-duck.jpg');
+const dragon = new Image('dragon', './img/dragon.jpg');
+const pen = new Image('pen', './img/pen.jpg');
+const petSweep = new Image('pet-sweep', './img/pet-sweep.jpg');
+const scissors = new Image('scissors', './img/scissors.jpg');
+const shark = new Image('shark', './img/shark.jpg');
+const sweep = new Image('sweep', './img/sweep.png');
+const tauntaun = new Image('tauntaun', './img/tauntaun.jpg');
+const unicorn = new Image('unicorn', './img/unicorn.jpg');
+const waterCan = new Image('water-can', './img/water-can.jpg');
+const wineGlass = new Image('wine-glass', './img/wine-glass.jpg');
+console.log(generatedImages);
 function getRandomImages() {
 
   while (selectedImages.length < imagesToDisplay) {
-    const randomIndex = Math.floor(Math.random() * Image.generatedImages.length);
-    const randomImage = Image.generatedImages[randomIndex];
+    // console.log('inside a while loop')
+    const randomIndex = Math.floor(Math.random() * generatedImages.length);
+    const randomImage = generatedImages[randomIndex];
 
     if (!selectedImages.includes(randomImage)) {
       selectedImages.push(randomImage);
@@ -57,6 +57,7 @@ function displayImages() {
   getRandomImages();
   const imageSection = document.getElementById('images');
   imageSection.innerHTML = '';
+  imageSection.addEventListener('click', handleImageClick);
 
   selectedImages.forEach(product => {
     const imgElement = document.createElement('img');
@@ -71,33 +72,32 @@ function displayImages() {
 
 function handleImageClick(event) {
   event.preventDefault();
+  console.log('image was clicked', event);
   const targetName = event.target.alt;
-  const foundObject = selectedImages.find(obj => obj.name === targetName);
-  
+  const foundObject = generatedImages.find(obj => obj.name === targetName);
+  console.log(selectedImages);
+  console.log(generatedImages);
+  console.log(foundObject);
+
   if (roundsDisplayed > 0) {
       foundObject.timesClicked++;
       roundsDisplayed--; // Decreasing the click limit by 1
       console.log(`Remaining clicks: ${roundsDisplayed}`);
-      
-      if (roundsDisplayed === 0) {
-          // Disable further clicks
-          console.log('Click limit reached. Further clicks disabled.');
-          disableClicks();
-      }
   } else {
       console.log('Click limit reached. Further clicks disabled.');
-      disableClicks();
+      // disableClicks();
   }
   
   selectedImages = [];
   displayImages();
   console.log(foundObject.timesClicked);
 }
-function disableClicks() {
-  const images = document.querySelectorAll('.product-image'); //The problem lies here. Replace with image class name.
-  images.forEach(image => {
-      image.removeEventListener('click', handleImageClick);
-  });
-}
+
+// function disableClicks() {
+//   const images = document.querySelectorAll('.product-image'); //The problem lies here. Replace with image class name.
+//   images.forEach(image => {
+//       image.removeEventListener('click', handleImageClick);
+//   });
+// }
 
 displayImages();
